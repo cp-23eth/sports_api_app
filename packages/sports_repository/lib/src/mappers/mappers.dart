d0d5cb08eb1c0dd2e168dea34b1106a81e33a850
Ethan Hofstetter <ethan.hofstetter@ceff.ch>
2025-02-07 10:33:33 +0100

    création models et mappers

--- packages/sports_repository/lib/src/mappers/mappers.dart
+++ packages/sports_repository/lib/src/mappers/mappers.dart
@@ -1,1 +1,4 @@
 export 'game_remote_rtdb_model_to_domain.dart';
+export 'player_remote_rtdb_model_to_domain.dart';
+export 'team_remote_rtdb_model_to_domain.dart';
+export 'stadium_remote_rtdb_model_to_domain.dart';
--- packages/sports_repository/lib/src/models/models.dart
+++ packages/sports_repository/lib/src/models/models.dart
@@ -1,1 +1,4 @@
 export 'game_remote_rtdb_model.dart';
+export 'player_remote_rtdb_model.dart';
+export 'team_remote_rtdb_model.dart';
+export 'stadium_remote_rtdb_model.dart';
