e34951463ea43438a04484b3e2b4d4b4a94944bf
Ethan Hofstetter <ethan.hofstetter@ceff.ch>
2025-02-07 15:30:25 +0100

    screeen

--- packages/features/sports_list/lib/src/screens/home_screen_nba.dart
+++ packages/features/sports_list/lib/src/screens/home_screen_nba.dart
@@ -1,4 +1,5 @@
 import 'package:component_library/component_library.dart';
+import 'package:domain_entities/domain_entities.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:sports_list/sports_list.dart';
@@ -40,7 +41,8 @@ class _HomeScreenNbaState extends State<HomeScreenNba> {
         mainAxisAlignment: MainAxisAlignment.start,
         mainAxisSize: MainAxisSize.max,
         children: [
-          // HomeMatchList(),
+          // TODO: mettre des games
+          HomeMatchList(finish: true, game: Game(gameId: 99, season: 2925, status: "final", isClosed: true, dateTime: "2024-10-22T19:30:00", dateTimeUtc: "2024-10-22T23:30:00", awayTeam: "BOS", awayTeamId: 44, homeTeam: "CHI", homeTeamId: 34, awayTeamScore: 45, homeTeamScore: 45, stadiumId: 45, refereeId: 45),),
         ],
       ),
     );
