e4002b989eb3a62422d80ffefbdf88b229bd3e5a
Quentin <quentin.mafille@ceff.ch>
2025-02-07 13:57:35 +0100

    + header

--- packages/component_library/lib/src/header.dart
+++ packages/component_library/lib/src/header.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Header extends StatelessWidget {
   const Header({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 75,
-      decoration: const BoxDecoration(
-        color: Color(0xFF1C5D99),
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(10),
-          bottomRight: Radius.circular(10),
-        ),
-        image: DecorationImage(
-          image: AssetImage(
-            'packages/component_library/lib/src/assets/images/nba_logo.png',
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 100,
+          decoration: const BoxDecoration(
+            color: Color(0xFF1C5D99),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Image.asset(
+                  'packages/component_library/lib/src/assets/images/nba_logo.png',
+                  height: 80,
+                  filterQuality: FilterQuality.high,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 32.0),
+                child: Text(
+                  'NBA',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: [],
-      ),
+      ],
     );
   }
 }
