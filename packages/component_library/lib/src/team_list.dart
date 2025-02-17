5e0429a147d61f4117459617ce88a67a279c4e55
Quentin <quentin.mafille@ceff.ch>
2025-02-17 14:53:47 +0100

    Refactor titles to center alignment and increase font sizes; implement SVG fetching and cleaning in TeamList

--- packages/component_library/lib/src/team_list.dart
+++ packages/component_library/lib/src/team_list.dart
@@ -2,34 +2,71 @@ import 'package:domain_entities/domain_entities.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
 class TeamList extends StatelessWidget {
   const TeamList({required this.team, super.key});
 
   final Team team;
 
+  Future<String> _fetchAndCleanSvg(String url) async {
+    // Télécharger le SVG
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      // Récupérer le contenu SVG en tant que chaîne
+      String svgContent = response.body;
+
+      // Nettoyer le SVG : supprimer la balise <style />
+      svgContent = svgContent.replaceAll(RegExp(r'<style.*?>.*?</style>'), '');
+
+      // Retourner le contenu nettoyé
+      return svgContent;
+    } else {
+      throw Exception("Échec du téléchargement du fichier SVG");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SvgPicture.network(
-          team.wikipediaLogoUrl,
-          height: 84,
-          width: 84,
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 48.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FutureBuilder<String>(
+              future: _fetchAndCleanSvg(team.wikipediaLogoUrl),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                }
+
+                if (snapshot.hasError) {
+                  return const Text('Erreur de chargement SVG');
+                }
+
+                return SvgPicture.string(
+                  snapshot.data!,
+                  width: 58.0,
+                  fit: BoxFit.fitWidth,
+                );
+              },
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Text(
+              '${team.city} ${team.name}',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontFamily: GoogleFonts.poppins().fontFamily,
+              ),
+            ),
+          ],
         ),
-        const SizedBox(
-          width: 32,
-        ),
-        Text(
-          '${team.city} ${team.name}',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontFamily: GoogleFonts.poppins().fontFamily,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
--- packages/features/sports_list/lib/src/screens/home_screen_nba.dart
+++ packages/features/sports_list/lib/src/screens/home_screen_nba.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:sports_list/sports_list.dart';
 import 'package:component_library/component_library.dart';
@@ -7,11 +6,28 @@ class HomeScreenNba extends StatefulWidget {
   const HomeScreenNba({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _HomeScreenNbaState createState() => _HomeScreenNbaState();
 }
 
 class _HomeScreenNbaState extends State<HomeScreenNba> {
+  // Pour gérer l'état des sections ouvertes/fermées
+  Map<String, bool> sectionsVisibility = {
+    'EAST': false,
+    'WEST': false,
+    'Atlantic': false,
+    'Central': false,
+    'South East': false,
+    'North West': false,
+    'Pacific': false,
+    'South West': false,
+  };
+
+  void toggleSection(String section) {
+    setState(() {
+      sectionsVisibility[section] = !sectionsVisibility[section]!;
+    });
+  }
+
   int _selectedIndex = 1;
 
   @override
@@ -96,86 +112,159 @@ class _HomeScreenNbaState extends State<HomeScreenNba> {
     );
   }
 
-  List<Widget> _widgetOptions(SportsListState state) => [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            const Header(),
-            Expanded(
-              child: ListView(
-                children: [
-                  const SecondaryTitle(text: 'Demain'),
-                  CalenderMatchList(game: state.games[1200]),
-                  CalenderMatchList(game: state.games[1201]),
-                  CalenderMatchList(game: state.games[1202]),
-                  const SecondaryTitle(text: 'Aujourd\'hui'),
-                  CalenderMatchList(game: state.games[821]),
-                  const SecondaryTitle(text: 'Hier'),
-                  CalenderMatchList(game: state.games[2]),
-                ],
-              ),
+  List<Widget> _widgetOptions(SportsListState state) {
+    return [
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          const Header(),
+          Expanded(
+            child: ListView(
+              children: [
+                const SecondaryTitle(text: 'Demain'),
+                CalenderMatchList(game: state.games[1200]),
+                CalenderMatchList(game: state.games[1201]),
+                CalenderMatchList(game: state.games[1202]),
+                const SecondaryTitle(text: 'Aujourd\'hui'),
+                CalenderMatchList(game: state.games[821]),
+                const SecondaryTitle(text: 'Hier'),
+                CalenderMatchList(game: state.games[2]),
+              ],
             ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Header(),
-            Expanded(
-              child: ListView.separated(
-                separatorBuilder: (context, index) => const SizedBox(
-                  height: 5,
-                ),
-                itemCount: 6,
-                itemBuilder: (context, index) {
-                  final date = DateTime.now();
-                  final comingGames = state.games
-                      .where(
-                          (game) => DateTime.parse(game.dateTime).isAfter(date))
-                      .toList();
-                  final finishGames = state.games
-                      .where((game) =>
-                          DateTime.parse(game.dateTime).isBefore(date))
-                      .toList();
-                  comingGames.sort((a, b) => DateTime.parse(a.dateTime)
-                      .compareTo(DateTime.parse(b.dateTime)));
+          ),
+        ],
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Header(),
+          Expanded(
+            child: ListView.separated(
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 5,
+              ),
+              itemCount: 6,
+              itemBuilder: (context, index) {
+                final date = DateTime.now();
+                final comingGames = state.games
+                    .where(
+                        (game) => DateTime.parse(game.dateTime).isAfter(date))
+                    .toList();
+                final finishGames = state.games
+                    .where(
+                        (game) => DateTime.parse(game.dateTime).isBefore(date))
+                    .toList();
+                comingGames.sort((a, b) => DateTime.parse(a.dateTime)
+                    .compareTo(DateTime.parse(b.dateTime)));
 
-                  if (index == 0) {
-                    return HomeMatchList(
-                      teams: state.teams,
-                      game: comingGames[2],
-                      finish: false,
-                    );
-                  } else if (index == 1) {
-                    return HomeMatchList(
-                      teams: state.teams,
-                      game: comingGames[1],
-                      finish: false,
-                    );
-                  } else if (index == 2) {
-                    return HomeMatchList(
-                      teams: state.teams,
-                      game: comingGames[0],
-                      finish: false,
-                    );
-                  } else if (index > 2 && index < 6) {
-                    return HomeMatchList(
-                      teams: state.teams,
-                      game: finishGames[index - 3],
-                      finish: true,
-                    );
-                  }
-                },
-              ),
+                if (index == 0) {
+                  return HomeMatchList(
+                    teams: state.teams,
+                    game: comingGames[2],
+                    finish: false,
+                  );
+                } else if (index == 1) {
+                  return HomeMatchList(
+                    teams: state.teams,
+                    game: comingGames[1],
+                    finish: false,
+                  );
+                } else if (index == 2) {
+                  return HomeMatchList(
+                    teams: state.teams,
+                    game: comingGames[0],
+                    finish: false,
+                  );
+                } else if (index > 2 && index < 6) {
+                  return HomeMatchList(
+                    teams: state.teams,
+                    game: finishGames[index - 3],
+                    finish: true,
+                  );
+                }
+              },
             ),
-          ],
+          ),
+        ],
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          const Header(),
+          Expanded(
+            child: ListView(
+              children: [
+                const SizedBox(height: 10),
+                buildSection('EAST', [
+                  buildSubSection('Atlantic', 5, 9),
+                  buildSubSection('Central', 10, 14),
+                  buildSubSection('South East', 0, 4),
+                ]),
+                const SizedBox(height: 40),
+                buildSection('WEST', [
+                  buildSubSection('North West', 15, 19),
+                  buildSubSection('Pacific', 25, 29),
+                  buildSubSection('South West', 20, 24),
+                ]),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ];
+  }
+
+  Widget buildSection(String title, List<Widget> subSections) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () => toggleSection(title),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              title,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
         ),
-        const Text(
-          'Index 2: School',
+        if (sectionsVisibility[title]!)
+          Column(
+            children: subSections,
+          ),
+      ],
+    );
+  }
+
+  Widget buildSubSection(String subTitle, int startIndex, int endIndex) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () => toggleSection(subTitle),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              subTitle,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
         ),
-      ];
+        if (sectionsVisibility[subTitle]!)
+          Column(
+            children: List.generate(endIndex - startIndex + 1, (index) {
+              return TeamList(
+                  team: context
+                      .read<SportsListProvider>()
+                      .state
+                      .teams[startIndex + index]);
+            }),
+          ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
