45673c31bdc4deb7fb93159adbb432ea40ba81e3
Ethan Hofstetter <ethan.hofstetter@ceff.ch>
2025-02-18 16:14:44 +0100

    ajout des images de simon

--- packages/component_library/lib/src/player_title.dart
+++ packages/component_library/lib/src/player_title.dart
@@ -1,5 +1,6 @@
 import 'package:domain_entities/domain_entities.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class PlayerTitle extends StatelessWidget {
@@ -9,27 +10,64 @@ class PlayerTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      mainAxisSize: MainAxisSize.max,
+    String cleanFirstName(String firstName) {
+      firstName = firstName.replaceAll('-', '');
+      firstName = firstName.replaceAll(' ', '');
+      firstName = firstName.replaceAll('.', '');
+      return firstName[0].toUpperCase() + firstName.substring(1).toLowerCase();
+    }
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Text(
-          '${player.firstName} ${player.lastName}',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontFamily: GoogleFonts.poppins().fontFamily,
-            fontWeight: FontWeight.w900,
+        CircleAvatar(
+          child: ClipOval(
+            child: SizedBox(
+              width: 180,
+              height: 180,
+              child: Image.asset(
+                'packages/component_library/lib/src/assets/images/player_images/${cleanFirstName(player.firstName)}-${player.lastName}.jpg',
+                width: 180,
+                height: 180,
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
+                errorBuilder: (context, error, stackTrace) {
+                  return SvgPicture.asset(
+                    'packages/component_library/lib/src/assets/images/player_images/user.svg',
+                    fit: BoxFit.cover,
+                    alignment: Alignment.topCenter,
+                  );
+                },
+              ),
+            ),
           ),
         ),
-        const SizedBox(width: 64),
-        Text(
-          _haveJersey(player) ? player.jersey.toString() : 'N/A',
-          style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontFamily: GoogleFonts.poppins().fontFamily,
-              fontWeight: FontWeight.w900),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              '${player.firstName} ${player.lastName}',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontFamily: GoogleFonts.poppins().fontFamily,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            const SizedBox(width: 64),
+            Text(
+              _haveJersey(player) ? player.jersey.toString() : 'N/A',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontFamily: GoogleFonts.poppins().fontFamily,
+                  fontWeight: FontWeight.w900),
+            ),
+          ],
         ),
       ],
     );
