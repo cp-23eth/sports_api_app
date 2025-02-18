2704f80bd42a6706f253cc392c2bb13f5525d6bc
Ethan Hofstetter <ethan.hofstetter@ceff.ch>
2025-02-18 09:40:41 +0100

    ajout du carrousel

--- packages/features/sports_list/lib/src/screens/home_screen.dart
+++ packages/features/sports_list/lib/src/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:component_library/component_library.dart';
+import 'package:domain_entities/domain_entities.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sports_list/sports_list.dart';
@@ -14,7 +16,38 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final state = context.watch<SportsListProvider>().state;
-    
+
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
+    final yesterday = today.subtract(const Duration(days: 1));
+    final tomorrow = today.add(const Duration(days: 1));
+   
+    final nextGames = state.games
+        .where((game) =>
+            game.isClosed == false)
+        .toList();
+
+    final lastGames = state.games
+        .where((game) =>
+            game.isClosed == true)
+        .toList();
+
+    nextGames.sort((a, b) => DateTime.parse(b.dateTime).compareTo(DateTime.parse(a.dateTime)));
+    lastGames.sort((a, b) => DateTime.parse(b.dateTime).compareTo(DateTime.parse(a.dateTime)));
+
+    final next8Games = nextGames.take(8).toList();
+    final last8Games = lastGames.take(8).toList();
+
+    final List<Widget> nextGamesList = [
+      for (var game in next8Games)
+        HomeMatchList(teams: state.teams, finish: false, game: game),
+    ];
+
+    final List<Widget> lastGamesList = [
+      for (var game in last8Games)
+        HomeMatchList(teams: state.teams, finish: true, game: game),
+    ];
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
@@ -32,17 +65,14 @@ class _HomeScreenState extends State<HomeScreen> {
                     fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 6),
-              HomeMatchList(
-                teams: state.teams,
-                game: state.games
-                    .where((game) =>
-                        DateTime.parse(game.dateTime).isAfter(DateTime.now()))
-                    .reduce((a, b) => DateTime.parse(a.dateTime)
-                            .isBefore(DateTime.parse(b.dateTime))
-                        ? a
-                        : b),
-                finish: false,
-              ),
+              CarouselSlider(items: nextGamesList, options: CarouselOptions(
+                  height: 180,
+                  enableInfiniteScroll: false,
+                  viewportFraction: 0.8,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 6),
+                  enlargeCenterPage: true,
+                ),),
               const SizedBox(height: 20),
               const Text(
                 'Résultat dernier match',
@@ -52,13 +82,16 @@ class _HomeScreenState extends State<HomeScreen> {
                     fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 6),
-              HomeMatchList(
-                teams: state.teams,
-                game: state.games
-                    .where((game) =>
-                        DateTime.parse(game.dateTime).isBefore(DateTime.now()))
-                    .first,
-                finish: true,
+              CarouselSlider(
+                items: lastGamesList,
+                options: CarouselOptions(
+                  height: 180,
+                  enableInfiniteScroll: false,
+                  viewportFraction: 0.8,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 6),
+                  enlargeCenterPage: true,
+                ),
               ),
             ],
           ),
--- packages/features/sports_list/lib/src/screens/screens.dart
+++ packages/features/sports_list/lib/src/screens/screens.dart
@@ -5,5 +5,3 @@ export 'players_screen.dart';
 export 'calendar_screen.dart';
 export 'home_screen_teams.dart';
 export 'home_screen.dart';
-
-export 'carousel_test.dart';
