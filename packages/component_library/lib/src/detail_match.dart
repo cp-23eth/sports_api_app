c3680bdb5fc32ad6271227ab6b7bf77a50fc9112
Quentin <quentin.mafille@ceff.ch>
2025-02-21 09:46:40 +0100

    Enhance DetailMatch layout by adjusting image dimensions and adding team names for improved clarity

--- packages/component_library/lib/src/detail_match.dart
+++ packages/component_library/lib/src/detail_match.dart
@@ -37,15 +37,21 @@ class DetailMatch extends StatelessWidget {
             children: [
               Column(
                 children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        'packages/component_library/lib/src/assets/images/svg/${teams[game.homeTeamId - 1].logo}',
-                        width: 87,
-                        fit: BoxFit.fitWidth,
-                      ),
-                    ],
+                  SvgPicture.asset(
+                    'packages/component_library/lib/src/assets/images/svg/${teams[game.homeTeamId - 1].logo}',
+                    height: 87,
+                    fit: BoxFit.fitHeight,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    teams[game.homeTeamId - 1].name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: GoogleFonts.poppins().fontFamily,
+                    ),
                   ),
                 ],
               ),
@@ -56,7 +62,7 @@ class DetailMatch extends StatelessWidget {
                 children: [
                   const SizedBox(height: 30),
                   const PrimaryTitle(text: 'VS'),
-                  const SizedBox(height: 10),
+                  const SizedBox(height: 50),
                   SecondaryTitle(
                       text: '${game.homeTeamScore} - ${game.awayTeamScore}'),
                   const SizedBox(height: 20),
@@ -80,12 +86,22 @@ class DetailMatch extends StatelessWidget {
                 ],
               ),
               Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   SvgPicture.asset(
                     'packages/component_library/lib/src/assets/images/svg/${teams[game.awayTeamId - 1].logo}',
-                    width: 87,
-                    fit: BoxFit.fitWidth,
+                    height: 87,
+                    fit: BoxFit.fitHeight,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    teams[game.awayTeamId - 1].name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: GoogleFonts.poppins().fontFamily,
+                    ),
                   ),
                 ],
               ),
