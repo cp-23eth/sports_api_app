96f5adaee549efa111e1a500ea43794f41c70040
Quentin <quentin.mafille@ceff.ch>
2025-02-21 08:44:08 +0100

    Update match list and calendar screens for improved layout and data handling

--- packages/features/sports_list/lib/src/screens/calendar_screen.dart
+++ packages/features/sports_list/lib/src/screens/calendar_screen.dart
@@ -1,39 +1,43 @@
 import 'package:component_library/component_library.dart';
 import 'package:domain_entities/domain_entities.dart';
 import 'package:flutter/material.dart';
-import 'package:sports_list/sports_list.dart';
 import 'package:intl/intl.dart';
 
 class CalendarScreen extends StatefulWidget {
-  const CalendarScreen({super.key});
+  const CalendarScreen(
+      {required this.stateTeams,
+      required this.stateStadiums,
+      required this.stateGames,
+      super.key});
+
+  final List<Team> stateTeams;
+  final List<Stadium> stateStadiums;
+  final List<Game> stateGames;
 
   @override
   State<CalendarScreen> createState() => _CalendarScreenState();
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
-  // bool _isLoading = true;
+  // @override
+  // void initState() {
+  //   super.initState();
+  // _loadData();
+  // }
 
-  @override
-  void initState() {
-    super.initState();
-    _loadData();
-  }
-
-  Future<void> _loadData() async {
-    if (context.read<SportsListProvider>().state.games.isEmpty) {
-      WidgetsBinding.instance.addPostFrameCallback(
-        (_) {
-          context.read<SportsListProvider>().fetchAndSetSports();
-        },
-      );
-    }
-  }
+  // Future<void> _loadData() async {
+  //   if (context.read<SportsListProvider>().state.games.isEmpty) {
+  //     WidgetsBinding.instance.addPostFrameCallback(
+  //       (_) {
+  //         context.read<SportsListProvider>().fetchAndSetSports();
+  //       },
+  //     );
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final state = context.watch<SportsListProvider>().state;
-    final games = state.games.reversed.toList();
+    final games = widget.stateGames.reversed.toList();
     final today = DateTime.now();
 
     // Group games by date
@@ -56,41 +60,39 @@ class _CalendarScreenState extends State<CalendarScreen> {
       }
     }
 
-    return state.status == SportsListStatus.loading
-        ? const Center(child: CircularProgressIndicator())
-        : Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              const Header(),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: groupedGames.length,
-                  itemBuilder: (context, index) {
-                    final date = groupedGames.keys.elementAt(index);
-                    final formattedDate =
-                        DateFormat('dd MMM yyyy').format(DateTime.parse(date));
-                    final gamesForDate = groupedGames[date]!;
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SecondaryTitle(text: formattedDate),
-                        ...gamesForDate.map(
-                          (game) => CalenderMatchList(
-                            teams: state.teams,
-                            game: game,
-                            stadiums: state.stadiums,
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                  controller: ScrollController(
-                      initialScrollOffset: initialScrollIndex *
-                          75.0), // Adjust the offset as needed
-                ),
-              )
-            ],
-          );
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        const Header(),
+        Expanded(
+          child: ListView.builder(
+            itemCount: groupedGames.length,
+            itemBuilder: (context, index) {
+              final date = groupedGames.keys.elementAt(index);
+              final formattedDate =
+                  DateFormat('dd MMM yyyy').format(DateTime.parse(date));
+              final gamesForDate = groupedGames[date]!;
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SecondaryTitle(text: formattedDate),
+                  ...gamesForDate.map(
+                    (game) => CalenderMatchList(
+                      teams: widget.stateTeams,
+                      game: game,
+                      stadiums: widget.stateStadiums,
+                    ),
+                  ),
+                ],
+              );
+            },
+            controller: ScrollController(
+                initialScrollOffset:
+                    initialScrollIndex * 75.0), // Adjust the offset as needed
+          ),
+        )
+      ],
+    );
   }
 }
