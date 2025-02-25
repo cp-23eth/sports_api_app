6b17ee40dba48f8ada55afbc670808ea26d62b38
Quentin <quentin.mafille@ceff.ch>
2025-02-25 13:32:33 +0100

    Enhance FavoriteTeamListSettings to support application color changes and improve team selection UI

--- packages/features/sports_list/lib/src/screens/parameter.dart
+++ packages/features/sports_list/lib/src/screens/parameter.dart
@@ -73,161 +73,170 @@ class _ParameterState extends State<Parameter> {
         ),
         backgroundColor: Parameter.headerFooterColor,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Expanded(
+        child: ListView(
           children: [
-            FavoriteTeamListSettings(teamList: teamList),
-            const PrimaryTitle(text: 'Change application colors'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  width: 200,
-                  child: SecondaryTitle(text: 'Header / Footer :'),
-                ),
-                const SizedBox(width: 10),
-                ButtonColorPicker(
-                  pickerColor: headerFooterColor,
-                  changeColor: changeColorHeaderFooter,
-                  keyColor: Parameter.headerFooterColor,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  width: 200,
-                  child: SecondaryTitle(text: 'Comings Matchs :'),
-                ),
-                const SizedBox(width: 10),
-                ButtonColorPicker(
-                  pickerColor: comingsMatchsColor,
-                  changeColor: changeColorComingsMatchs,
-                  keyColor: Parameter.comingsMatchsColor,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  width: 200,
-                  child: SecondaryTitle(text: 'Latests Matchs :'),
-                ),
-                const SizedBox(width: 10),
-                ButtonColorPicker(
-                  pickerColor: latestsMatchsColor,
-                  changeColor: changeColorLatestsMatchs,
-                  keyColor: Parameter.latestsMatchsColor,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  width: 200,
-                  child: SecondaryTitle(text: 'Todays Matchs :'),
-                ),
-                const SizedBox(width: 10),
-                ButtonColorPicker(
-                  pickerColor: todaysMatchsColor,
-                  changeColor: changeColorTodaysMatchs,
-                  keyColor: Parameter.todaysMatchsColor,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  width: 200,
-                  child: SecondaryTitle(text: 'Teams Headers :'),
-                ),
-                const SizedBox(width: 10),
-                ButtonColorPicker(
-                  pickerColor: teamsHeaderColor,
-                  changeColor: changeColorTeamsHeader,
-                  keyColor: Parameter.teamsHeaderColor,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  width: 200,
-                  child: SecondaryTitle(text: 'Teams Players :'),
-                ),
-                const SizedBox(width: 10),
-                ButtonColorPicker(
-                  pickerColor: teamsPlayersColor,
-                  changeColor: changeColorTeamsPlayers,
-                  keyColor: Parameter.teamsPlayersColor,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  width: 200,
-                  child: SecondaryTitle(text: 'Teams Stadiums :'),
-                ),
-                const SizedBox(width: 10),
-                ButtonColorPicker(
-                  pickerColor: teamsStadiumColor,
-                  changeColor: changeColorTeamsStadium,
-                  keyColor: Parameter.teamsStadiumColor,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  width: 200,
-                  child: SecondaryTitle(text: 'Backgrounds :'),
-                ),
-                const SizedBox(width: 10),
-                ButtonColorPicker(
-                  pickerColor: backgroundColor,
-                  changeColor: changeBackgroundColor,
-                  keyColor: Parameter.backgroundColor,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    WidgetStatePropertyAll(Parameter.headerFooterColor),
-              ),
-              onPressed: () {
-                changeColorHeaderFooter(const Color(0xFF1C5D99));
-                changeColorComingsMatchs(const Color(0xFFC8102E));
-                changeColorLatestsMatchs(const Color(0xFF1D428A));
-                changeColorTodaysMatchs(const Color(0xFF9100B6));
-                changeColorTeamsHeader(const Color(0xFF1C5D99));
-                changeColorTeamsStadium(const Color(0xFFC8102E));
-                changeColorTeamsPlayers(const Color(0xFF9100B6));
-                changeBackgroundColor(const Color(0xFF0B132B));
-              },
-              child: Text(
-                'Reset',
-                style: TextStyle(
-                  color: ThemeData.estimateBrightnessForColor(
-                              Parameter.backgroundColor) ==
-                          Brightness.light
-                      ? Colors.black
-                      : Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const PrimaryTitle(text: 'Change application colors'),
+                  FavoriteTeamListSettings(
+                      teamList: teamList,
+                      removeApplicationColor: removeApplicationColor,
+                      changeApplicationColor: changeApplicationColor),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 200,
+                        child: SecondaryTitle(text: 'Header / Footer :'),
+                      ),
+                      const SizedBox(width: 10),
+                      ButtonColorPicker(
+                        pickerColor: headerFooterColor,
+                        changeColor: changeColorHeaderFooter,
+                        keyColor: Parameter.headerFooterColor,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 200,
+                        child: SecondaryTitle(text: 'Comings Matchs :'),
+                      ),
+                      const SizedBox(width: 10),
+                      ButtonColorPicker(
+                        pickerColor: comingsMatchsColor,
+                        changeColor: changeColorComingsMatchs,
+                        keyColor: Parameter.comingsMatchsColor,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 200,
+                        child: SecondaryTitle(text: 'Latests Matchs :'),
+                      ),
+                      const SizedBox(width: 10),
+                      ButtonColorPicker(
+                        pickerColor: latestsMatchsColor,
+                        changeColor: changeColorLatestsMatchs,
+                        keyColor: Parameter.latestsMatchsColor,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 200,
+                        child: SecondaryTitle(text: 'Todays Matchs :'),
+                      ),
+                      const SizedBox(width: 10),
+                      ButtonColorPicker(
+                        pickerColor: todaysMatchsColor,
+                        changeColor: changeColorTodaysMatchs,
+                        keyColor: Parameter.todaysMatchsColor,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 200,
+                        child: SecondaryTitle(text: 'Teams Headers :'),
+                      ),
+                      const SizedBox(width: 10),
+                      ButtonColorPicker(
+                        pickerColor: teamsHeaderColor,
+                        changeColor: changeColorTeamsHeader,
+                        keyColor: Parameter.teamsHeaderColor,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 200,
+                        child: SecondaryTitle(text: 'Teams Players :'),
+                      ),
+                      const SizedBox(width: 10),
+                      ButtonColorPicker(
+                        pickerColor: teamsPlayersColor,
+                        changeColor: changeColorTeamsPlayers,
+                        keyColor: Parameter.teamsPlayersColor,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 200,
+                        child: SecondaryTitle(text: 'Teams Stadiums :'),
+                      ),
+                      const SizedBox(width: 10),
+                      ButtonColorPicker(
+                        pickerColor: teamsStadiumColor,
+                        changeColor: changeColorTeamsStadium,
+                        keyColor: Parameter.teamsStadiumColor,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 200,
+                        child: SecondaryTitle(text: 'Backgrounds :'),
+                      ),
+                      const SizedBox(width: 10),
+                      ButtonColorPicker(
+                        pickerColor: backgroundColor,
+                        changeColor: changeBackgroundColor,
+                        keyColor: Parameter.backgroundColor,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          WidgetStatePropertyAll(Parameter.headerFooterColor),
+                    ),
+                    onPressed: () {
+                      changeColorHeaderFooter(const Color(0xFF1C5D99));
+                      changeColorComingsMatchs(const Color(0xFFC8102E));
+                      changeColorLatestsMatchs(const Color(0xFF1D428A));
+                      changeColorTodaysMatchs(const Color(0xFF9100B6));
+                      changeColorTeamsHeader(const Color(0xFF1C5D99));
+                      changeColorTeamsStadium(const Color(0xFFC8102E));
+                      changeColorTeamsPlayers(const Color(0xFF9100B6));
+                      changeBackgroundColor(const Color(0xFF0B132B));
+                    },
+                    child: Text(
+                      'Reset',
+                      style: TextStyle(
+                        color: ThemeData.estimateBrightnessForColor(
+                                    Parameter.backgroundColor) ==
+                                Brightness.light
+                            ? Colors.black
+                            : Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
@@ -289,6 +298,61 @@ class _ParameterState extends State<Parameter> {
     setState(() {
       backgroundColor = color;
       Parameter.backgroundColor = color;
+    });
+  }
+
+  void changeApplicationColor(Team team) {
+    final teamColors = {
+      'Knicks': [0xFFF58426, 0xFF1D428A, 0xFF9EA2A2, 0xFFF58426],
+      '76ers': [0xFFC4CED4, 0xFF002B5C, 0xFFC8102E, 0xFFC4CED4],
+      'Nets': [0xFF858585, 0xFF75899F, 0xFF627E75, 0xFF000000],
+      'Celtics': [0xFF008348, 0xFF759F8D, 0xFFBB9753, 0xFF772A00],
+      'Raptors': [0xFFCE1141, 0xFF777777, 0xFF262626, 0xFFCE1141],
+      'Bulls': [0xFFCE1114, 0xFF777777, 0xFF262626, 0xFFCE1114],
+      'Cavaliers': [0xFFBC945C, 0xFF777777, 0xFF262626, 0xFF860038],
+      'Pacers': [0xFFFDBB30, 0xFFBEC0C2, 0xFF002D62, 0xFFFDBB30],
+      'Pistons': [0xFF1D428A, 0xFFBEC0C2, 0xFFC8102E, 0xFF1D428A],
+      'Bucks': [0xFF00471B, 0xFF0077C0, 0xFFEEE1C6, 0xFF00471B],
+      'Wizards': [0xFF002B5C, 0xFFC4CED4, 0xFFE31837, 0xFF002B5C],
+      'Hornets': [0xFF00788C, 0xFFA1A1A4, 0xFF1D1160, 0xFF00788C],
+      'Hawks': [0xFFC8102E, 0xFF9EA2A2, 0xFFFDB927, 0xFFC8102E],
+      'Heat': [0xFF98002E, 0xFFF9A01B, 0xFFF9A01B, 0xFF98002E],
+      'Magic': [0xFF0077C0, 0xFFC4CED4, 0xFFC4CED4, 0xFF0077C0],
+      'Timberwolves': [0xFF0C233F, 0xFF266092, 0xFF266092, 0xFF79BC43],
+      'Jazz': [0xFF002144, 0xFF274E37, 0xFFFFA200, 0xFF002144],
+      'Thunder': [0xFF002D62, 0xFFFDBB30, 0xFFEF3B24, 0xFF007AC1],
+      'Trail Blazers': [0xFFE03A3E, 0xFFC10005, 0xFFC10005, 0xFFE03A3E],
+      'Nuggets': [0xFF0E2240, 0xFF8B2131, 0xFFFEC524, 0xFF244289],
+      'Warriors': [0xFF1D428A, 0xFFFDB927, 0xFFFDB927, 0xFF1D428A],
+      'Lakers': [0xFF31006F, 0xFFFDB927, 0xFFFDB927, 0xFF31006F],
+      'Clippers': [0xFF12173F, 0xFF4891CE, 0xFFC8102E, 0xFF12173F],
+      'Suns': [0xFF1D1160, 0xFFE56020, 0xFFE56020, 0xFF1D1160],
+      'Kings': [0xFF5A2B81, 0xFF707271, 0xFF707271, 0xFF5A2B81],
+      'Grizzlies': [0xFF12173F, 0xFF5D76A9, 0xFF5D76A9, 0xFF12173F],
+      'Rockets': [0xFFCE1141, 0xFF373A36, 0xFF9EA2A2, 0xFFCE1141],
+      'Pelicans': [0xFF0C2340, 0xFFCE0E2D, 0xFFCE0E2D, 0xFFB9975B],
+      'Spurs': [0xFFC4CED4, 0xFF919DA5, 0xFF919DA5, 0xFFC4CED4],
+      'Mavericks': [0xFF0064B1, 0xFFBBC4CA, 0xFFBBC4CA, 0xFF0064B1],
+    };
+
+    if (teamColors.containsKey(team.name)) {
+      final colors = teamColors[team.name]!;
+      setState(() {
+        Parameter.headerFooterColor = Color(colors[0]);
+        Parameter.comingsMatchsColor = Color(colors[1]);
+        Parameter.latestsMatchsColor = Color(colors[2]);
+        Parameter.todaysMatchsColor = Color(colors[3]);
+      });
+    }
+  }
+
+  void removeApplicationColor() {
+    setState(() {
+      Parameter.headerFooterColor = const Color(0xFF1C5D99);
+      Parameter.comingsMatchsColor = const Color(0xFFC8102E);
+      Parameter.latestsMatchsColor = const Color(0xFF1D428A);
+      Parameter.todaysMatchsColor = const Color(0xFF9100B6);
+      Parameter.backgroundColor = const Color(0xFF0B132B);
     });
   }
 }
