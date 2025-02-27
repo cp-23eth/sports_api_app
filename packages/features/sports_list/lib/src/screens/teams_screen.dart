d762e48259ecadb2b74cf3bfcffc02059c77d992
Ethan Hofstetter <ethan.hofstetter@ceff.ch>
2025-02-27 20:40:26 +0100

    ajout circleAvatar

--- packages/features/sports_list/lib/src/screens/teams_screen.dart
+++ packages/features/sports_list/lib/src/screens/teams_screen.dart
@@ -45,6 +45,9 @@ class _TeamsScreenState extends State<TeamsScreen> {
     final Color secondaryColor =
         Color(int.parse('0xFF${widget.team.secondaryColor}'));
 
+    // String design = 'CircleAvatar';
+    String design = 'list';
+
     return Scaffold(
       backgroundColor: Parameter.backgroundColor,
       appBar: AppBar(
@@ -152,16 +155,52 @@ class _TeamsScreenState extends State<TeamsScreen> {
               const SizedBox(
                 height: 16.0,
               ),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: filteredPlayers.length,
-                itemBuilder: (context, index) => PlayerList(
-                  user: widget.user,
-                  player: filteredPlayers[index],
-                  color: secondaryColor,
-                ),
-              ),
+              design == 'CircleAvatar'
+                  ? ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: filteredPlayers.length,
+                      itemBuilder: (context, index) => PlayerList(
+                        user: widget.user,
+                        player: filteredPlayers[index],
+                        color: secondaryColor,
+                      ),
+                    )
+                  : Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24.0, vertical: 8.0),
+                      child: GridView.builder(
+                        shrinkWrap: true, // Ajouté
+                        physics: const NeverScrollableScrollPhysics(), // Ajouté
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 50,
+                          mainAxisSpacing: 50,
+                        ),
+                        itemCount: filteredPlayers.length,
+                        itemBuilder: (context, index) {
+                          final player = filteredPlayers[index];
+                          return Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              CircleAvatar(
+                                minRadius: 50,
+                                backgroundImage: AssetImage(
+                                    'packages/component_library/lib/src/assets/images/player_images/${player.firstName}-${player.lastName}.jpg'),
+                              ),
+                              Text(
+                                player.firstName + ' ' + player.lastName,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
             ],
           ),
         ],
